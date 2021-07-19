9ff1c44a0971bab48e23f0bd210c9a3f73f4a847
jackcornblum <jcornblum@gmail.com>
2021-07-19 12:01:00 -0400

    commit tree 818e6cc58ce5cc1d828a263b492846421792b0b3

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,17 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.grades
+    grades = Student.all.map do |student|
+      student.grade
+    end
+
+    sorted_grades = grades.sort! {|x, y| y <=> x}
+    sorted_students = sorted_grades.map do |grade|
+      Student.find_by_grade(grade)
+    end
+
+    sorted_students
+  end
+
 end
